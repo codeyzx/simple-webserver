f44f256e7fa1d865dd38ca1aa64e7742b1db1b78
codeyzx <yahyatruth@gmail.com>
2024-11-21 14:40:26 +0700

    init commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# HTTP Server Project
+
+This project implements a simple HTTP server in C. It includes functionalities for routing, parameter parsing, and serving static files.
+
+## Visualization of this repo
+
+![Visualization of this repo](./diagram.svg)
+
+## Features
+
+- **HTTP Server Initialization**: Initialize and configure the server to listen on a specified port.
+- **Routing**: Define and manage routes to serve different HTML files.
+- **Parameter Parsing**: Parse URL parameters and manage them dynamically.
+- **Static File Serving**: Serve static files such as CSS.
+
+## Files
+
+- **HTTP_Server.c**: Contains functions to initialize the server, set status codes, and set response bodies.
+- **Routes.c**: Manages routes using a binary search tree structure.
+- **Parameters.c**: Handles dynamic parameter arrays, including initialization, addition, sorting, and retrieval.
+- **main.c**: The main entry point of the server, handling client connections and requests.
+
+## Authors
+
+1. Yahya Alfon Sinaga (231524064)
+2. Amadeus (231524059)
---
+++ src/main.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include "HTTP_Server.h"
+#include "Routes.h"
+#include "Response.h"
+#include "Parameters.h"
+
+int main() {
+	// initiate HTTP_Server
+	HTTP_Server http_server;
+	init_server(&http_server, 6969);
+
+	int client_socket;
+	
+	// registering Routes
+	struct Route * route = initRoute("/", "index.html"); 
+	addRoute(&route, "/about", "about.html");
+	addRoute(&route, "/sth", "sth.html");
+	addRoute(&route, "/chicken", "chicken.html");
+
+	// create dynamic parameters array
+	struct ParameterArray * params = paramInit(10);
+
+
+	printf("\n====================================\n");
+	printf("=========ALL AVAILABLE ROUTES========\n");
+	// display all available routes
+	inorder(route);
+
+	while (1) {
+		char client_msg[4096] = "";
+
+		client_socket = accept(http_server.socket, NULL, NULL);
+
+		read(client_socket, client_msg, 4095);
+		printf("%s\n", client_msg);
+
+		// parsing client socket header to get HTTP method, route
+		char *method = "";
+		char *urlRoute = "";
+
+		char *client_http_header = strtok(client_msg, "\n");
+			
+		printf("\n\n%s\n\n", client_http_header);
+
+		char *header_token = strtok(client_http_header, " ");
+		
+		int header_parse_counter = 0;
+
+		while (header_token != NULL) {
+
+			switch (header_parse_counter) {
+				case 0:
+					method = header_token;
+				case 1:
+					urlRoute = header_token;
+			}
+			header_token = strtok(NULL, " ");
+			header_parse_counter++;
+		}
+
+		printf("The method is %s\n", method);
+		printf("The route is %s\n", urlRoute);
+
+		if (strlen(urlRoute) > 0)
+		{
+			paramParse(params, urlRoute);
+
+			printf("*** PARSED PARAMETERS ***\n");
+			for (size_t i = 0; i < params->n_members; ++i)
+			{
+				printf("KEY: %s\n", params->parameters[i].key);
+				printf("VALUE: %s\n\n", params->parameters[i].value);
+			}
+			// do something with parameters and can clear them to re-use for the next
+			// request
+			paramClear(params);
+		}
+
+
+		char template[100] = "";
+		
+		if (strstr(urlRoute, "/static/") != NULL) {
+			//strcat(template, urlRoute+1);
+			strcat(template, "static/index.css");
+			http_set_status_code(&http_server, OK);
+		}else {
+			struct Route * destination = search(route, urlRoute);
+			strcat(template, "templates/");
+
+			if (destination == NULL) {
+				strcat(template, "404.html");
+				http_set_status_code(&http_server, NOT_FOUND);
+			}else {
+				strcat(template, destination->value);
+				http_set_status_code(&http_server, OK);
+			}
+		}
+
+		char * response_data = render_static_file(template);
+		http_set_response_body(&http_server, response_data);
+
+		send(client_socket, http_server.response, sizeof(http_server.response), 0);
+		close(client_socket);
+		free(response_data);
+	}
+	return 0;
+}
